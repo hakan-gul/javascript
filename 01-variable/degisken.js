54792fd76b1c2031b290e3fb76c36617372e455b
Hakan <hakan.gul@daimia.com>
2022-11-02 15:55:22 +0300

    js

---
+++ 01-variable/degisken.js
@@ -0,0 +1,32 @@
+var urunAdi = "iphone 13";
+let urunFiyat= 15000;
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat );
+ 
+// let sayi1="10";
+// let sayi2="20";
+
+// console.log(Number(sayi1)+Number(sayi2));
+
+let sayi1=10;
+let sayi2=20;
+
+console.log(sayi1.toString() + sayi2.toString());
+
+let isim ="sena";
+let soyad ="turan";
+
+console.log(isim +" "+ soyad);
+
+let sinavNotu= 70;
+let basarilimi = (sinavNotu >= 50);
+
+console.log(basarilimi);
+
+let yas = "";
+console.log(yas);
+console.log(typeof yas);
+
+yas== undefined;
+
